566537d41de4408f89383f36c3bc139163b060c9
Aaron Graham <aarongraham9@gmail.com>
2014-01-28 15:06:08 -0400

    Adding Project Info and Debug print flag.
    Adding Project Info and Debug print flag.

--- src/com/Kronius/moved/MoveD.java
+++ src/com/Kronius/moved/MoveD.java
@@ -4,44 +4,69 @@ import java.io.File;
 
 public class MoveD {
 
+	private static final boolean DEBUG_MODE = true;
+	
 	private static String DEFAULT_SRC = File.separator + "media" + File.separator + "PiDataDrive" + File.separator + "Downloads" + File.separator + "Done" + File.separator;
 	private static String DEFAULT_DEST = File.separator + "media" + File.separator + "MediaDrive" + File.separator + "Video" + File.separator + "New" + File.separator;
 	private String src = "";
 	private String dest = "";
 	
+//	public MoveD(){
+//		//TODO: XML Parsing for paths
+//		
+//		MoveD(DEFAULT_SRC, DEFAULT_DEST);
+//	}
+	
 	public MoveD(String srcIn, String destIn){
 		src = srcIn;
 		dest = destIn;
 		//TODO: Setup logging
+		//TODO: XML Parsing
 	}
 	
 	public void move(){
-		boolean done = false;
+		
+		boolean done = false;//TODO: Store done flag in XML File?
 		
 		do{
 			try{
+				File srcDirectory = new File(src);
 				
-				File afile = new File(src);
-			 
-				if(afile.renameTo(new File(dest + afile.getName()))){
-					System.out.println("File is moved successful!");
-				}else{
-					System.out.println("File is failed to move!");
+				//Get list of all files in the folder
+				
+				String[] fileList = srcDirectory.list();
+				
+				for(String fileName: fileList){
+					System.out.println(fileName);
 				}
+				
+				//---------------------------------------------------------------------------------------------------------------------------------
+				
+				
+			 
+//				if(srcDirectory.renameTo(new File(dest + srcDirectory.getName()))){
+//					System.out.println("File is moved successful!");
+//				}else{
+//					System.out.println("File is failed to move!");
+//				}
 			 
 	    	}catch(Exception e){
 	    		e.printStackTrace();
 	    	}
 			
+			done = true;
+			
 		}while(!done);
 	}
 	
 	public static void main(String[] args){
+		MoveD moveD;
+		
 		//TODO: Remove Test Paths
 		String srcIn = "C:\\Users\\CASA\\moved\\testdir\\" + DEFAULT_SRC;
-		String destIn = "C:\\Users\\CASA\\moved\\testdir\\" + DEFAULT_DEST;
+		String destIn = "C:\\Users\\CASA\\moved\\testdir\\" + DEFAULT_DEST;	
+		moveD = new MoveD(srcIn, destIn);
 		
-		MoveD moveD = new MoveD(srcIn, destIn);
 		moveD.move();
 	}
 }
